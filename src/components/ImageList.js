385ac173f239687bde99da25e965eea9222fd26e
Gmrf18 <gmrf18@gmail.com>
2019-01-20 11:35:49 -0600

    terminamos app de imagenes

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,10 +1,11 @@
 import './ImageList.css'
 import React from 'react';
+import ImageCard from './ImageCard'
 
 const ImageList = ( props ) => {
     
-    const images =  props.images.map(({ id, description, urls}) => {
-    return <img key={id} alt={description} src={urls.regular}/>
+    const images =  props.images.map((image) => {
+    return <ImageCard key={image.id} image={image}/>
     })
 
     return (
